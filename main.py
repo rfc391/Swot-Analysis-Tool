cfa9d32a6ec98838235200b0b168db656bc59155
Robert Shubert <robs@apexsecurityint.com>
2024-12-24 11:45:58 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-
-import sys
-from api.api import app
-from gui.gui import FileApp
-
-def main():
-    if len(sys.argv) > 1 and sys.argv[1] == "api":
-        app.run(host="0.0.0.0", port=5000)
-    else:
-        import tkinter as tk
-        root = tk.Tk()
-        app = FileApp(root)
-        root.mainloop()
-
-if __name__ == "__main__":
-    main()
+<Content of enhanced main.py from previous step>
